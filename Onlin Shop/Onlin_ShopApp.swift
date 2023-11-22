939a36966e49a134184f68ddb1b228a3dab4a4e0
Artavazd Kahchatryan <artavazd575@gmail.com>
2023-11-22 20:55:57 +0400

    Initial Commit

---
+++ Onlin Shop/Onlin_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Onlin_ShopApp.swift
+//  Onlin Shop
+//
+//  Created by Artavazd Khachatryan on 22.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Onlin_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
